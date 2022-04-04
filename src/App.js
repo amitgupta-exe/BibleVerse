e794bc1c52357e142854f97c20a3e9da0d87ae11
Amit Gupta <mail.amit85764@gmail.com>
2022-04-04 12:41:09 +0530

    Pushing for GitHub Pages \

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import books from 'biblejs/lib/books';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+    //State
+    const [data, setData] = useState([]);
+
+    //Generating Random book, chapter, verse.
+    const book = books[Math.floor(Math.random() * books.length)];
+    const bookName = book.names[0];
+    const chapter = Math.floor(Math.random() * book.verses.length) + 1;
+    const verse = Math.floor(Math.random() * book.verses[chapter - 1]) + 1;
+
+    //API Call Format
+    const api = "https://bible-api.com";
+    var url = `${api}/${bookName} ${chapter}:${verse}`;
+
+    //Default Verse
+    useEffect(async () => {
+        const response = await fetch("https://bible-api.com/genesis 1:3");
+        setData(await response.json());
+    }, []);
+
+    //Update Verse On Click
+    const handleClick = async () => {
+        const response = await fetch(url);
+        setData(await response.json());
+    }
+
+    //HTML
+    return (
+        <main onClick={handleClick}>
+            <div className="container">
+                <div className='text'>
+                    <h3 className='info'>{data.reference}</h3>
+                    <p className='text'>{data.text}</p>
+                </div>
+            </div>
+        </main>
+    )
 }
 
 export default App;
